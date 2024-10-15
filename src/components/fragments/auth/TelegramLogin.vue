7f78b7098c24a7032a75c9b415646d071b90cf21
Ivan Korol <invariant0x@icloud.com>
2024-10-15 21:51:51 +0300

    telegram

--- src/components/fragments/auth/TelegramLogin.vue
+++ src/components/fragments/auth/TelegramLogin.vue
@@ -61,6 +61,14 @@ const initTelegramWebApp = () => {
   loading.value = true;
   errorMessage.value = '';
 
+  const urlParams = new URLSearchParams(window.location.search);
+
+// Извлекаем параметр code
+  const code = urlParams.get('code');
+
+  alert( urlParams);
+
+
   if (window.Telegram && window.Telegram.WebApp) {
     initData.value = window.Telegram.WebApp.initData; // Сырой initData
 
